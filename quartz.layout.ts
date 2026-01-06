c76427a65acb406fe03eba47aeb0a6852f65cfb9
Stephanie Conrad <stephnoraconrad@gmail.com>
2026-01-06 15:47:16 -0700

    Remove Backlinks component from sidebar

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -50,7 +50,6 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
   right: [],
 }
