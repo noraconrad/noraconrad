2adf3ca8110de5383d882cb6a1ce5d91654e73e1
Stephanie Conrad <stephnoraconrad@gmail.com>
2026-01-06 15:01:38 -0700

    Remove ContentMeta to hide date and reading time from headers

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -19,7 +19,6 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
@@ -50,7 +49,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
