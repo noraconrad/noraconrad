f05b44d7fc5a27688f455c76368988e9a865603b
Stephanie Conrad <stephnoraconrad@gmail.com>
2026-01-06 15:06:38 -0700

    Change typography: Playfair Display italic for headers, Helvetica Neue for body

--- quartz.config.ts
+++ quartz.config.ts
@@ -21,8 +21,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Playfair Display",
+        body: "Helvetica Neue",
         code: "IBM Plex Mono",
       },
       colors: {
